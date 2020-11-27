0afa398726a204625136d838cb06eb4342bba385
RollsRoycewk <50765560@qq.com>
2020-11-27 19:25:38 +0800

    feat:header路由完成

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,21 +1,112 @@
 <template>
-  <div class="header">
-    Header...
-    <p>header childer</p>
-  </div>
+  <header class="header">
+    <div class="top">
+      <div class="container">
+        <div class="loginList">
+          <p>尚品汇欢迎您!</p>
+          <p>
+            <span>请</span>
+            <router-link to="/login">登录</router-link>
+            <a href="###" class="resgis">免费注册</a>
+          </p>
+        </div>
+        <div class="infoNav">
+          <a href="###">我的订单</a>
+          <a href="###">我的购物车</a>
+          <a href="###">我的尚品汇</a>
+          <a href="###">尚品汇会员</a>
+          <a href="###">企业采购</a>
+          <a href="###">关注尚品汇</a>
+          <a href="###">合作招商</a>
+          <a href="###">商家后台</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="header-bottom">
+      <h1 class="logoSearch">
+        <a href="##">
+          <img src="./images/logo.png" alt="" />
+        </a>
+      </h1>
+      <div class="searchArea">
+        <form action="">
+          <input type="text" class="searchInput" placeholder="三星 苹果 锤子" />
+          <button class="searchBtn" @click="handleSearch">搜索</button>
+        </form>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
 export default {
   name: "Header",
+  methods: {
+    handleSearch() {
+      this.$router.push("/search");
+    },
+  },
 };
 </script>
 
 <style lang="less" scoped>
-.header {
-  background: green;
-  p {
-    font-size: 30px;
+.top {
+  // height: 30px;
+  background-color: #eaeaea;
+}
+.container {
+  width: 1200px;
+  height: 30px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.loginList {
+  display: flex;
+  p:nth-child(1) {
+    margin-right: 10px;
   }
+}
+.resgis {
+  border-left: 1px solid #b3aeae;
+  margin-left: 5px;
+  padding-left: 5px;
+}
+.infoNav {
+  a {
+    padding: 0 10px;
+    border-right: 1px solid #b3aeae;
+  }
+  a:last-child {
+    border: none;
+  }
+}
+.header-bottom {
+  height: 106px;
+  width: 1200px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.searchInput {
+  text-indent: 10px;
+  box-sizing: border-box;
+  width: 490px;
+  height: 32px;
+  border: 2px solid #ea4a36;
+  outline: none;
+}
+.searchBtn {
+  width: 68px;
+  height: 32px;
+  background-color: #ea4a36;
+  border: none;
+  color: #fff;
+  outline: none;
+  cursor: pointer;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 
 import router from "./router";
+// 引入样式重置
+import "./styles/reset.css";
+
 Vue.config.productionTip = false;
 
 new Vue({
